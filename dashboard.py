b41b4e63127cc421255f35a0080a34c7bf52d23a
Stefan Crummenerl <Crummenerl@web.de>
2019-09-18 20:36:40 +0200

    add 1st basic callbacks for lat, lon and depth

--- dashboard.py
+++ dashboard.py
@@ -16,34 +16,53 @@ app = dash.Dash(__name__, external_stylesheets=EXTERNAL_STYLESHEETS)
 
 app.layout = html.Div(children=[
 
-    # row -------------------------------------------------------------------
+    # row ---------------------------------------------------------------------
     html.Div([
         html.H1('Make me that rock', style={'color': 'white'})
     ], className='row', style={'backgroundColor':'#1f2c56'}),
 
-    # row -------------------------------------------------------------------
+    # row ---------------------------------------------------------------------
     html.Div([
-        # col
+        # col ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         html.Div([
             html.Div(id='div_map_plot'),
-            dcc.RadioItems(
-                id='map_style',
-                options=[
-                    {"label": "basic", "value": "basic"},
-                    {"label": "satellite", "value": "satellite"},
-                    {"label": "outdoors", "value": "outdoors"},
-                ],
-                value='basic',
-                labelStyle={'display': 'inline-block'}
-            ),
-        ],  className="eight columns", style={'backgroundColor':'#1f2c56'}),
-        # col
+            html.Div([
+                html.Div([
+                    dcc.RadioItems(
+                        id='map_style',
+                        options=[
+                            {"label": "basic", "value": "basic"},
+                            {"label": "satellite", "value": "satellite"},
+                            {"label": "outdoors", "value": "outdoors"},
+                        ],
+                        value='basic',
+                        labelStyle={'display': 'inline-block'},                    
+                    ),
+                ], className='four columns'),
+                html.Div(id='selected_coordinates', className='eight columns')
+            ], style={'color': 'white'}),
+        ], className="nine columns", style={'backgroundColor':'#1f2c56'}),
+        # col ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         html.Div([
-            html.H1('Selectors', style={'color': 'white'})
-        ],  className="four columns", style={'backgroundColor':'yellow'}),
-    ], className='row', style={'backgroundColor':'blue'}),
+            dcc.Input(
+                id='input_lat',
+                type='number',
+                placeholder='Latitude'
+            ),
+            dcc.Input(
+                id='input_lon',
+                type='number',
+                placeholder='Longitude'
+            ),
+            dcc.Input(
+                id='input_depth',
+                type='number',
+                placeholder='Depth'
+            ),
+        ], className="three columns", style={'backgroundColor':'#1f2c56'}),
+    ], className='row', style={'backgroundColor':'#1f2c56'}),
 
-    # row 
+    # row ---------------------------------------------------------------------
     html.Div([
         html.Div(
             html.H1('More selectors', style={'color': 'white'}),
@@ -93,6 +112,27 @@ def update_mapplot(map_style):
         }
 
         return dcc.Graph(figure={'data': data, 'layout': layout})
+
+@app.callback(
+    Output(component_id='selected_coordinates', component_property='children'),
+    [Input(component_id='input_lat', component_property='value'),
+     Input(component_id='input_lon', component_property='value'),
+     Input(component_id='input_depth', component_property='value')]
+)
+def update_show_coordinates(input_lat, input_lon, input_depth):
+
+    return html.Div([
+        html.Div([
+            html.P(f'Latitude: {input_lat}')
+        ], className="four columns"),
+        html.Div([
+            html.P(f'Latitude: {input_lon}')
+        ], className="four columns"),
+        html.Div([
+            html.P(f'Depth: {input_depth}')
+        ], className="four columns")
+    ])
+
 
 # Server ======================================================================
 if __name__ == '__main__':
